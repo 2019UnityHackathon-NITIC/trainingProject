5fb4ec335abbd33bdc744829b931c221ef7b5ca4
isso0424 <isso04240129@gmail.com>
2019-12-04 10:18:04 +0900

    create Shoot() and make do that press 'J'

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,11 +10,16 @@ public class Player : MonoBehaviour
     [SerializeField] private float _jumpFlag = 1.5f;
     [SerializeField] private GameObject spawnPoint;
     private Rigidbody2D rb;
+    static public bool _attackDirectionFlag; // true : front, false, back
+    [SerializeField] private GameObject gun;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject swapDirectionBullet;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         move = new Move(_moveSpeed, _jumpFlag, rb);
+        _attackDirectionFlag = true;
     }
 
     // Update is called once per frame
@@ -27,9 +32,18 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) direction.Add(1);
         move.move(direction);
         if (Input.GetKey(KeyCode.Space)) move.jump(isGround);
+        if (direction.IndexOf(1) != -1 && direction.IndexOf(0) == -1) _attackDirectionFlag = false;
+        else if(direction.IndexOf(0) != -1 && direction.IndexOf(1) == -1) _attackDirectionFlag = true;
+        if (Input.GetMouseButtonDown(0)) Shoot();
     }
 
     void Death(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Shoot(){
+        Vector3 shooter = gun.transform.position;
+        if (_attackDirectionFlag) Instantiate(bullet, shooter, Quaternion.identity);
+        else Instantiate(swapDirectionBullet, shooter, Quaternion.identity);
     }
 }
