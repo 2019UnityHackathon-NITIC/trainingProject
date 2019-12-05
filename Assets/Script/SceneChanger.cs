c2be73aaea1f44020f931260b28afa7b0ddf31d4
Colk-tech <mk239312@icloud.com>
2019-12-05 16:09:53 +0900

    Added provisional title scene

---
+++ Assets/Script/SceneChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void StartGame()
+    {
+        string mainScene = "SampleScene";
+        SceneManager.LoadScene(mainScene);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+}
